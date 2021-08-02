2aea4257008ea38e17234f851cef5c85f19769bf
Stephen Nielson <snielson@discoverandchange.com>
2021-08-02 13:07:06 -0400

    Implement ResponseInterface for API

--- src/CustomSkeletonAPI.php
+++ src/CustomSkeletonAPI.php
@@ -4,6 +4,9 @@
 namespace OpenEMR\Modules\CustomModuleSkeleton;
 
 
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ResponseInterface;
+
 /**
  * Class CustomSkeletonAPI  is an example to illustrate adding api endpoints to the system.  Note these are not intended
  * to demonstrate FHIR compliance as they return simple JSON objects.
@@ -11,19 +14,27 @@ namespace OpenEMR\Modules\CustomModuleSkeleton;
  */
 class CustomSkeletonAPI
 {
-    public function listResources() {
-        
+    public function listResources() : ResponseInterface {
         $resources = [
             ['_id' => 1 ,'_message' => 'This is resource 1']
             ,['_id' => 2 ,'_message' => 'This is resource 2']
             ,['_id' => 3, '_message' => 'This is resource 3']
         ];
-        http_response_code(200);
-        echo json_encode($resources);
+
+        return $this->createResponseForArray($resources);
     }
 
-    public function getOneResource() {
-        http_response_code(200);
-        echo json_encode(['_id' => 1, '_message'])
+    public function getOneResource() : ResponseInterface {
+        $data = ['_id' => 1, '_message'];
+        return $this->createResponseForArray($data);
+    }
+
+    private function createResponseForArray(array $resourceData)
+    {
+        $psr17Factory = new Psr17Factory();
+
+        $responseBody = $psr17Factory->createStream(json_encode($resourceData));
+        $response = $psr17Factory->createResponse(200)->withBody($responseBody);
+        return $response;
     }
 }
