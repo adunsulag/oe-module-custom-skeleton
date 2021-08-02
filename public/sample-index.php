088b0768e30192be3d2be8d1b83abc4356f09179
Stephen Nielson <snielson@discoverandchange.com>
2021-08-02 10:58:08 -0400

    Initial OpenEMR Module Skeleton Project

---
+++ public/sample-index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Sample HTML page
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ *
+ * @author    Stephen Nielson <stephen@nielson.org>
+ * @copyright Copyright (c) 2021 Stephen Nielson <stephen@nielson.org>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+?>
+<html>
+<body>
+<h1>OpenEMR Custom Skeleton Index page</h1>
+<p>Congratulations you have installed and activated your OpenEMR module.  You can replace this page with anything you would like.</p>
+</body>
+</html>
---
+++ src/Bootstrap.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Bootstrap custom module skeleton.  This file is an example custom module that can be used
+ * to create modules that can be utilized inside the OpenEMR system.  It is NOT intended for
+ * production and is intended to serve as the barebone requirements you need to get started
+ * writing modules that can be installed and used in OpenEMR.
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ *
+ * @author    Stephen Nielson <stephen@nielson.org>
+ * @copyright Copyright (c) 2021 Stephen Nielson <stephen@nielson.org>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+namespace OpenEMR\Modules\CustomModuleSkeleton;
+
+class Bootstrap
+{
+    const MODULE_INSTALLATION_PATH = "";
+    const MODULE_NAME = "oe-module-custom-skeleton";
+	/**
+	 * @var EventDispatcher The object responsible for sending and subscribing to events through the OpenEMR system
+	 */
+	private $eventDispatcher;
+
+	public function __construct(EventDispatcher $eventDispatcher)
+	{
+	}
+
+	public function subscribeToEvents()
+	{
+		$this->addGlobalSettings();
+		$this->registerMenuItems();
+		$this->subscribeToApiEvents();
+	}
+
+	public function addGlobalSettings()
+	{
+	}
+
+	public function registerMenuItems()
+	{
+		/**
+		 * @var EventDispatcherInterface $eventDispatcher
+		 * @var array                    $module
+		 * @global                       $eventDispatcher @see ModulesApplication::loadCustomModule
+		 * @global                       $module          @see ModulesApplication::loadCustomModule
+		 */
+		$eventDispatcher->addListener(MenuEvent::MENU_UPDATE, [$this, 'addCustomModuleMenuItem']);
+	}
+
+	public function addCustomModuleMenuItem(MenuEvent $event)
+	{
+	    $menu = $event->getMenu();
+
+	    $menuItem = new stdClass();
+	    $menuItem->requirement = 0;
+	    $menuItem->target = 'mod';
+	    $menuItem->menu_id = 'mod0';
+	    $menuItem->label = xlt("Custom Module Skeleton");
+	    // TODO: pull the install location into a constant into the codebase so if OpenEMR changes this location it
+        // doesn't break any modules.
+	    $menuItem->url = "/interface/modules/custom_modules/oe-module-custom-skeleton/public/sample-module.php";
+	    $menuItem->children = [];
+
+	    /**
+	     * This defines the Access Control List properties that are required to use this module.
+	     * Several examples are provided
+	     */
+	    $menuItem->acl_req = [];
+
+	    /**
+	     * If you would like to restrict this menu to only logged in users who have access to see all user data
+	     */
+	    //$menuItem->acl_req = ["admin", "users"];
+
+	    /**
+	     * If you would like to restrict this menu to logged in users who can access patient demographic information
+	     */
+	    //$menuItem->acl_req = ["users", "demo"];
+
+
+	    /**
+	     * This menu flag takes a boolean property defined in the $GLOBALS array that OpenEMR populates.
+	     * It allows a menu item to display if the property is true, and be hidden if the property is false
+	     */
+	    //$menuItem->global_req = ["custom_skeleton_module_enable"];
+
+	    /**
+	     * If you want your menu item to allows be shown then leave this property blank.
+	     */
+	    $menuItem->global_req = [];
+
+	    foreach ($menu as $item) {
+		if ($item->menu_id == 'modimg') {
+		    $item->children[] = $menuItem;
+		    break;
+		}
+	    }
+
+	    $event->setMenu($menu);
+
+	    return $event;
+	}
+
+	public function subscribeToApiEvents() {
+
+	}
+}
